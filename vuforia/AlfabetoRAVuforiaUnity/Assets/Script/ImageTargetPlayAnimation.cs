239c4849b8f2125103e93ead5a61feca5b1a2bbf
jcsuscriptor <jcsuscriptor@gmail.com>
2018-08-17 21:58:51 -0500

    Actualizar Repositorio con cambios

---
+++ vuforia/AlfabetoRAVuforiaUnity/Assets/Script/ImageTargetPlayAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetPlayAnimation : MonoBehaviour, ITrackableEventHandler
+{
+
+    public Animator Animacion;
+    public string NombreAnimacion;
+
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+	 
+	
+	 
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+           newStatus == TrackableBehaviour.Status.TRACKED ||
+           newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            Animacion.Play(NombreAnimacion);
+        }
+        else
+        {
+            // Stop audio when target is lost
+            Animacion.Play("none");
+        }
+    }
+}
